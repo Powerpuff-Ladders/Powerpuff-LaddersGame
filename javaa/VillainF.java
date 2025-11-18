ccb5428f69333065fc0f0252b6ca66402d40fad4
Crizzly Iya Sabellita <crizzlyiya.sabellita@cit.edu>
2025-11-18 09:36:10 +0800

    Delete javaa directory

--- javaa/VillainF.java
+++
@@ -1,8 +0,0 @@
-public class VillainF extends CharacterF {
-    String type;
-
-    public VillainF(String name, int health, String type) {
-        super(name, health);
-        this.type = type;
-    }
-}
